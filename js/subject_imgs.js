4eeae639286d51cf4e62bfc5b5040ac960805abe
mweaverdyck <18310669+mweaverdyck@users.noreply.github.com>
2018-09-26 22:30:21 -0700

    Reduced number of nodes from 12 to 10

--- js/subject_imgs.js
+++ js/subject_imgs.js
@@ -1,1 +1,1 @@
-var subject_imgs = ["AF-203.png", "AF-234.png", "AM-228.png", "AM-243.png", "LF-213.png", "LF-229.png", "LF-230.png", "LM-211.png", "LM-225.png", "WF-039.png", "WM-214.png", "WM-230.png"];
+var subject_imgs = ["AF-203.png", "AF-234.png", "AM-228.png", "AM-243.png", "LF-213.png", "LF-229.png", "LM-211.png", "WF-039.png", "WM-214.png", "WM-230.png"];
